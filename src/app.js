9300a90b13a06de3d783e742b8b296440d5a58d1
Grant Houston <grant.houston44@hotmail.com>
2019-12-17 19:20:46 +0000

    finished first extension, can change currency to currency

--- src/app.js
+++ src/app.js
@@ -7,12 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
     data: {
       currencies: [],
       enteredEuros: 0,
-      selectedCurrency: null
+      selectedCurrency: null,
+      enteredCurrency: 0,
+      selectedCurrencyToEuros: null,
+      currency1: null,
+      enteredCurrency1: 0,
+      currency2: null,
     },
 
     computed: {
-      calculatedConversion: function(){
-        return this.calcConversion()
+      calculatedEurosConversion: function(){
+        return this.calcConversion(this.enteredEuros, this.selectedCurrency)
+      },
+      calculatedCurrencyConversion: function(){
+        return this.calcConversionToEuros(this.enteredCurrency, this.selectedCurrencyToEuros)
+      },
+      calculcatedCurrencyToCurrency: function(){
+        let step1 = this.calcConversionToEuros(this.enteredCurrency1,this.currency1)
+        return this.calcConversion(step1,this.currency2);
       }
     },
 
@@ -26,11 +38,20 @@ mounted(){
         .then(currencies => this.currencies = currencies.rates)
       },
 
-      calcConversion: function(){
-        let result = this.enteredEuros * this.selectedCurrency
+      calcConversion: function(amount, chosenCurrency){
+        let result = amount * chosenCurrency
         return result.toFixed(2);
-      }
+      },
+      calcConversionToEuros: function(amount, chosenCurrency) {
+        let result = amount / chosenCurrency
+        return result.toFixed(2)
+      },
+      // calcCurrencyToCurrency:function(){
+      //   calcconversion
+      //   return result.toFixed(2);
+      // }
     }
+
 
 
 
